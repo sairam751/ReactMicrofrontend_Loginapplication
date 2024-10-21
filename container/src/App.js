0e1914b05caf375c4beeb99f01a13c5e1c2c6c56
sairam751 <sairam751348@gmail.com>
2024-10-21 18:59:43 +0530

    Handling css of different module such that not to override css of other modules

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from'./components/Header'
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
+    <BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
     <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
+      <Header />
+      <MarketingApp/>
     </div>
+    </StylesProvider>
+    </BrowserRouter>
   );
 };
