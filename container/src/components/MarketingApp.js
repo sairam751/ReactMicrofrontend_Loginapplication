23044353d5ec679ee56ced2674694346538ebe16
sairam751 <sairam751348@gmail.com>
2024-10-22 19:02:34 +0530

    Passing the history from marketing to container app

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,21 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current,{
+        onNavigate: ({pathname: nextPathName }) =>{
+            //console.log('The container noticed a navigation in marketing')
+            //console.log(nextPathName)
+            if (nextPathName !== history.location.pathname) {
+             history.push(nextPathName);
+            }
+        }
+    });
   });
 
   return <div ref={ref} />;
