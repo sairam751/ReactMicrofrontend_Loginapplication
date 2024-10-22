df97eeedbd32a77a7d5015ed826c77a27511f444
sairam751 <sairam751348@gmail.com>
2024-10-22 19:23:50 +0530

    Running m,arketing app in isolation for history object

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory,createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el,{ onNavigate }) => {
-    const history = createMemoryHistory();
+const mount = (el,{ onNavigate,defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+   // const browserHistory = createBrowserHistory();
     if(onNavigate)
     {
         history.listen(onNavigate);
@@ -32,7 +33,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot,{});
+    mount(devRoot,{ defaultHistory: createBrowserHistory()});
   }
 }
 
